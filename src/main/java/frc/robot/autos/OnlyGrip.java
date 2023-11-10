ffb3bec72efae9e66603412f899a4ce853598c29
FRC Team FMWill 7576 <yigit.olc@hotmail.com>
2023-11-11 00:14:44 +0300

    fixed only score auto

--- src/main/java/frc/robot/autos/OnlyGrip.java
+++ src/main/java/frc/robot/autos/OnlyGrip.java
@@ -4,20 +4,20 @@ package frc.robot.autos;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.ScoreHighCommand;
+import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Gripper;
 import frc.robot.subsystems.Swerve;
 
 public class OnlyGrip extends SequentialCommandGroup {
-  public OnlyGrip(Swerve s_Swerve, Gripper s_Gripper ) {
+  public OnlyGrip(Swerve s_Swerve, Gripper s_Gripper, Arm s_Arm ) {
 
     // An example trajectory to follow.  All units in meters.
 
     addCommands(
-      new InstantCommand(() -> s_Swerve.resetModulesToAbsolute()),
-      new InstantCommand(() -> s_Swerve.zeroGyro()),
-      s_Gripper.run(() -> s_Gripper.outake()).withTimeout(1.1),
-      new InstantCommand(() -> s_Gripper.stop()),
-     new InstantCommand(() -> s_Swerve.drive(
+      new ScoreHighCommand(s_Arm, s_Gripper),
+      s_Arm.run(() -> s_Arm.armHome()).withTimeout(0.85),
+      new InstantCommand(() -> s_Swerve.drive(
             new Translation2d(0, 0),
             0,
             false,
